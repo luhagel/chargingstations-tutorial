e7a3f5f775b6125339768737b4a2d070e39f7f28
Luca Hagel <hagel.luca@gmail.com>
2017-02-15 23:57:07 -0800

    Request Current Position

--- react-native/app/screens/FindNearMe.js
+++ react-native/app/screens/FindNearMe.js
@@ -1,6 +1,10 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
+
+import config from '../config/config';
+
 import Container from '../components/Container';
 import { Header } from '../components/Text';
+import LocationButton from '../components/LocateMeButton';
 
 class FindNearMe extends Component {
   static route = {
@@ -9,9 +13,34 @@ class FindNearMe extends Component {
     },
   }
 
+  static propTypes = {
+    navigator: PropTypes.object,
+  }
+
+  static locationConfig = {
+    enableHighAccuracy: true,
+    timeout: 20000,
+    maximumAge: 1000,
+  }
+
+  getLocation = () => {
+    navigator.geolocation.getCurrentPosition(this.handleLocationSuccess,
+                                             this.handleLocationError,
+                                             this.locationConfig);
+  }
+
+  handleLocationError = (err) => {
+    this.props.navigator.showLocalAlert(err.message, config.errorStyles);
+  }
+
+  handleLocationSuccess = (res) => {
+    console.log(res);
+  }
+
   render() {
     return (
       <Container>
+        <LocationButton onPress={this.getLocation} />
         <Header>
           Find Near Me
         </Header>
