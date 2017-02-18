1c022a52321724fa187bb84557296cd75fa05970
Luca Hagel <hagel.luca@gmail.com>
2017-02-18 13:30:44 -0800

    checkin functionality

--- meteor/imports/api/locations/methods.js
+++ meteor/imports/api/locations/methods.js
@@ -1,3 +1,4 @@
+import { Meteor } from 'meteor/meteor';
 import { ValidatedMethod } from 'meteor/mdg:validated-method';
 import { SimpleSchema } from 'meteor/aldeed:simple-schema';
 
@@ -22,5 +23,35 @@ export const getNearestLocations = new ValidatedMethod({
         },
       },
     }, { limit: 10 }).fetch();
+  },
+});
+
+export const changeCheckinStatus = new ValidatedMethod({
+  name: 'Locations.changeCheckinStatus',
+  validate: new SimpleSchema({
+    locationId: { type: String },
+    status: { type: String, allowedValues: ['in', 'out'] },
+  }).validator(),
+  run({ locationId, status }) {
+    const location = Locations.findOne({ _id: locationId });
+
+    if (!location) {
+      throw new Meteor.Error('Locations.changeCheckin.invalidLocationId',
+        'Must pass a valid location id to change checkin status.');
+    }
+
+    if (status === 'in') {
+      Locations.update({ _id: locationId }, {
+        $set: {
+          checkedInUserId: 'demo',
+        },
+      });
+    } else {
+      Locations.update({ _id: locationId }, {
+        $set: {
+          checkedInUserId: null,
+        },
+      });
+    }
   },
 });
